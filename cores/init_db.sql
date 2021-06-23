e77b1a69ea57fb119a81e51bf52e81e9e62277df
long <nguyenphilong.bkevent@gmail.com>
2021-06-23 22:34:52 +0700

    update azure-db

--- cores/init_db.sql
+++ cores/init_db.sql
@@ -1,53 +1,47 @@
+-- Data table
+CREATE TABLE device (
+    dev_id int IDENTITY PRIMARY KEY,
+    feed_in varchar(1000),
+    feed_out VARCHAR(1000)
+);
+
+CREATE TABLE patient (
+    pat_id int identity primary KEY,
+    first_name VARCHAR(200),
+	last_name VARCHAR(200),
+	email VARCHAR(200),
+	phone VARCHAR(200),
+	created_on date,
+	modified_on date,
+    dev_id int, -- 1:1 to patient
+    CONSTRAINT fk_dev_id
+    FOREIGN KEY (dev_id) REFERENCES device(dev_id)
+);
+
+CREATE TABLE treatment (
+    treatment_id int identity PRIMARY KEY,
+    treatment_name VARCHAR(40),
+    treatment_time datetime,
+    treatment_desc varchar(1000),
+
+    pat_id int,
+    CONSTRAINT fk_pat_id1 -- M:1 to patient
+    FOREIGN KEY (pat_id) REFERENCES patient(pat_id),
+);
+
 CREATE TABLE temp_history (
-    devid int,
     recv_time datetime,
-    temp_value float
+    temp_value float,
+    
+    pat_id int,
+    CONSTRAINT fk_pat_id2
+    FOREIGN KEY (pat_id) REFERENCES patient(pat_id),
+    PRIMARY KEY(pat_id, recv_time)
 );
 
 CREATE TABLE qtyt (
-    id int,
-    warning_level int,
+    warning_level int PRIMARY KEY, -- use this field to check light color
     temp_from float,
     temp_to float,
-    time_from time
+    duration int
 );
-
-CREATE TABLE users(
-    id int,CREATE TABLE temp_history (
-        devid int,
-        recv_time datetime,
-        temp_value float
-    );
-    
-    CREATE TABLE qtyt (
-        id int,
-        warning_level int,
-        temp_from float,
-        temp_to float,
-        time_from time
-    );
-    
-    CREATE TABLE users(
-        id int,
-        name string
-    );
-    CREATE TABLE patient(
-        id int,
-        name string,
-        cmnd int,
-        address string,
-        room_number int,
-        phone_number string,
-        status string
-    );
-    -- name string
-);
--- CREATE TABLE patient(
---     id int,
---     name string,
---     cmnd int,
---     address string,
---     room_number int,
---     phone_number string,
---     status string
--- );
---
+++ cores/insert_data.sql
@@ -0,0 +1,18 @@
+insert into device values('','');
+insert into device values('','');
+insert into device values('3','');
+insert into device values('4','');
+insert into device values('5','');
+insert into device values('6','');
+insert into device values('pipe1404/feeds/led-zz', 'pipe1404/feeds/anotherfeed');
+
+insert into patient values('long','nguyen','malongnhan@gmail.com','0346156078','2020-12-12','2020-12-12', 7);
+
+insert into treatment(treatment_name, treatment_desc, pat_id) values ('do hematocrit1', 'fhewohoih',1);
+insert into treatment(treatment_name, treatment_desc, pat_id) values ('do than nhiet', 'fhewohoih',1);
+insert into treatment(treatment_name, treatment_desc, pat_id) values ('do duong huyet', 'fhewohoih',1);
+
+insert into qtyt VALUES (0,37, 37.5, 15);
+insert into qtyt VALUES (1,37.5, 38.5, 15);
+insert into qtyt VALUES (2,38.5, 39.5, 15);
+insert into qtyt VALUES (3,39.5, 41, 15);
